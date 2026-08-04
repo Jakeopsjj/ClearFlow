4c1d9937e96fa066ef06129913c7d5e3c98c580c
Jakeopsjj <Jakeopsjj@users.noreply.github.com>
2026-08-04 06:43:04 +0000

    chore: 版本号更新至 v1.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.cleardu.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.6.3"
+        versionCode = 12
+        versionName = "1.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
